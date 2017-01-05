ef0092ac86d7fce964ea2dc9eac5a7ece390261e
Theodore Yang <njuzheqin@gmail.com>
2017-01-05 01:22:20 -0500

    resign optimizer

--- src/Optimizer.cpp
+++ src/Optimizer.cpp
@@ -1,49 +1,167 @@
+#include <chrono>
 #include <algorithm>
+
 #include "Optimizer.h"
+#include "Expense.h"
 
 namespace AccountBalancer {
-    //this is supposed to be the settings
+    //optimizer implimentations
     struct BalanceOptimizer::BalanceOptimizerImpl {
         bool verbose = false;
-        std::vector<std::unique_ptr<BalanceMap>> history;
+        //the time point with high resolution, which is kept at record
+        //to see if we need to update optimization
+        std::chrono::time_point<std::chrono::system_clock> last_optimize_time = 
+            std::chrono::system_clock::now();
+
+        //a map from name to tansferSummary, support orderer output
+        std::map<std::string, TransferSummary> result;
     };
 
     BalanceOptimizer::BalanceOptimizer(): pimpl(std::make_unique<BalanceOptimizerImpl>()) {}
 
-    BalanceMap& BalanceOptimizer::optimizeTransactions(const BalanceMap& original_map) {
-        //sum up all the debt and get the total amount for each person
-        std::vector<std::pair<std::string, double>> personal_debts;
-        {
-            std::unordered_map<std::string, double> personal_debts_map;
-            for (const std::string& name: original_map.getAllPeople()) {
-                //if not found initialize to 0.0
-                if (personal_debts_map.find(name) == personal_debts_map.end()) {
-                    personal_debts_map[name] = 0.0;
-                }
+    bool BalanceOptimizer::isUpToTime (
+            const std::chrono::time_point<std::chrono::system_clock>& time_point) const {
+        return pimpl->last_optimize_time > time_point;
+    }
 
-                auto debts = original_map.getAllDebts(name);
-                double owing = 0.0;
-                for (auto it = debts->begin(), last = debts->end();
-                        it != last; ++it) {
-                    personal_debts_map[name] += it->second;
-                    personal_debts_map[it->first] = personal_debts_map.find(it->first) 
-                        == personal_debts_map.end()? -it->second: 
-                        personal_debts_map[it->first] - it->second;
-                }
-            }
-
-            //push everything into the vector and sort it by value
-            for (auto it = personal_debts_map.begin(), last = personal_debts_map.end();
+    OptimizerStatus BalanceOptimizer::optimizeExpenses(
+            const std::vector<std::shared_ptr<Expense>>& expenses) {
+        pimpl->result.clear();
+        //process each expenses
+        for (auto& expense: expenses) {
+            auto weight_map = expense->getWeightsMap();
+            double amount = expense->getAmount();
+            double weight_sum = static_cast<double> (expense->getWeightSum());
+            for (auto it = weight_map.begin(), last = weight_map.end();
                     it != last; ++it) {
-                if (!Utils::isEqual(it->second, 0.0)) {
-                    personal_debts.push_back(*it);
+                std::string name = it->first;
+                int share = it->second;
+                
+                if (pimpl->result.find(name) == pimpl->result.end()) {
+                    pimpl->result.emplace(name, name);
                 }
+                //this method might throw exception, but since we construct it before,
+                //it should be fine
+                pimpl->result.at(name).addExpense(expense);
+
+                //the expense need to add to everybody's account
+                pimpl->result.at(name).getTotalExpense() += amount * share / weight_sum;
             }
-            auto lambda_sort = [] (const std::pair<std::string, double>& p1, 
-                    const std::pair<std::string, double>& p2) {
-                return p1.second < p2.second;
-            };
-            std::sort(personal_debts.begin(), personal_debts.end(), lambda_sort);
+            //now the creditor of this expense has to be added into payment
+            auto creditor = expense->getCreditor();
+            if (pimpl->result.find(creditor) == pimpl->result.end()) {
+                pimpl->result.emplace(creditor, creditor);
+            }
+            pimpl->result.at(creditor).getPaymentMade() += amount;
+        }
+
+        //the strategy to optimize the gap
+        //"gap" is defined to be the difference between the amount this person should pay
+        //and the he actually paied
+        //overall, the gap must be 0, but we want to optimize such that the total number of
+        //transfers is minimum
+        std::vector<std::pair<std::string, double>> gaps;
+        gaps.reserve(pimpl->result.size());
+        //for each person, calculate gap and put it into the vector
+        for (auto it = pimpl->result.cbegin(), last = pimpl->result.cend();
+                it != last; ++it) {
+            auto name = it->first;
+            double payment_made = it->second.getPaymentMade();
+            double total_expense = it->second.getTotalExpense();
+            double gap = payment_made - total_expense;
+            gaps.push_back(std::make_pair(name, gap));
+        }
+        //sort the gaps, based on the gap value
+        auto comparator = [](const std::pair<std::string, double>& p1, 
+                const std::pair<std::string, double>& p2) -> bool {
+            return p1.second < p2.second;
+        };
+        std::sort(gaps.begin(), gaps.end(), comparator);
+        //use dfs to get the minimum amount of transfer
+
+        return OptimizerStatus::SUCCESS;
+    }
+
+    //transfer summay implementation
+    struct TransferSummary::TransferSummaryImpl {
+        //name of the person
+        std::string name;
+        //all the expenses that is engaged in, use weak pointer since it might dangle
+        std::vector<std::weak_ptr<Expense>> expenses;
+        //a list of transfers that are supposed to happen, it is a list of debt, also, use weak_ptr
+        std::vector<Transfer> transfers;
+        //total expense, this is the expense this person should make
+        double total_expense;
+        //total payment, this is the payment this person made at the first place
+        double payment_made;
+
+        //constructor
+        TransferSummaryImpl(const std::string& _name): name(_name), 
+                                                       total_expense(0.0),
+                                                       payment_made(0.0) {}
+
+        TransferSummaryImpl(std::string&& _name): name(std::move(_name)),
+                                                  total_expense(0.0),
+                                                  payment_made(0.0) {}
+    };
+
+    //ctors
+    TransferSummary::TransferSummary(const std::string& name):
+        pimpl(std::make_unique<TransferSummaryImpl>(name)) {}
+
+    TransferSummary::TransferSummary(std::string&& name):
+        pimpl(std::make_unique<TransferSummaryImpl>(std::move(name))) {}
+
+    //copy ctor and copy assignment operator
+    //note that this class have value semantics
+    TransferSummary::TransferSummary(const TransferSummary& _other):
+        pimpl(std::make_unique<TransferSummaryImpl>(*_other.pimpl)) {}
+
+    TransferSummary& TransferSummary::operator=(const TransferSummary& _other) {
+        //strong exception guarantee
+        using std::swap;
+        TransferSummary temp(_other);
+        swap(*this, temp);
+        return *this;
+    }
+
+    //dtor
+    TransferSummary::~TransferSummary() = default;
+
+    void TransferSummary::swap(TransferSummary& other) noexcept {
+        using std::swap;
+        swap(pimpl, other.pimpl);
+    }
+
+    //add one more expense
+    void TransferSummary::addExpense(std::shared_ptr<Expense> expense) {
+        if (expense) {
+            pimpl->expenses.push_back(std::weak_ptr<Expense>(expense));
         }
     }
+
+    //get total expense
+    double& TransferSummary::getTotalExpense() {
+        return pimpl->total_expense;
+    }
+
+    const double& TransferSummary::getTotalExpense() const {
+        return pimpl->total_expense;
+    }
+
+    //get payment made
+    double& TransferSummary::getPaymentMade() {
+        return pimpl->payment_made;
+    }
+
+    const double& TransferSummary::getPaymentMade() const {
+        return pimpl->payment_made;
+    }
 } //AccountBalancer
+
+namespace std {
+    void swap(AccountBalancer::TransferSummary& left, 
+            AccountBalancer::TransferSummary& right) noexcept {
+        left.swap(right);
+    }
+} //std
--- src/Optimizer.h
+++ src/Optimizer.h
@@ -2,11 +2,17 @@
 #define __BALANCE_OPTIMIZER_H
 #include <string>
 #include <vector>
+#include <chrono>
 
-#include "BalanceMap.h"
+#include "Expense.h"
 #include "utils.h"
 
 namespace AccountBalancer {
+    enum class OptimizerStatus {
+        SUCCESS,
+        FAILED
+    };
+
     class BalanceOptimizer {
     private:
         struct BalanceOptimizerImpl;
@@ -15,11 +21,85 @@ namespace AccountBalancer {
     public:
         BalanceOptimizer();
 
-        BalanceMap& optimizeTransactions(const BalanceMap& original_map);
+        //test whether this optimizer is a valid (newer than the given time)
+        bool isUpToTime(const std::chrono::time_point<std::chrono::system_clock>& time_point) const;
 
-        BalanceMap& getLatestOptimization() const;
+        //given a set of expenses, optimize it, return status code
+        OptimizerStatus optimizeExpenses(const std::vector<std::shared_ptr<Expense>>& expenses);
+
+        //output a summary of transfers
+        OptimizerStatus printAllTransfers() const;
+
+        //output a single person's transfers
+        OptimizerStatus printParticipantTransfers(const std::string& name) const;
+
+        //print a single person's expense summary
+        OptimizerStatus printParticipantSummary(const std::string& name) const;
     };
+
+    //a summary for a single participants containing
+    //1. what is the different expenses that they are engaged in
+    //2. who do you need to transfer based on the optimization
+    //3. what is the total amount of expense
+    class TransferSummary {
+    private:
+        struct TransferSummaryImpl;
+        std::unique_ptr<TransferSummaryImpl> pimpl;
+    public:
+        friend std::ostream& operator<<(std::ostream&, const TransferSummary&);
+
+        //ctors
+        TransferSummary(const std::string& name);
+
+        TransferSummary(std::string&& name);
+
+        //copy ctors and copy assignment operator
+        TransferSummary(const TransferSummary&);
+
+        TransferSummary& operator=(const TransferSummary&);
+
+        //dtor
+        ~TransferSummary();
+
+        //move ctors and move assignment sctor
+        TransferSummary(TransferSummary&&) = default;
+
+        TransferSummary& operator=(TransferSummary&&) = default;
+
+        void swap(TransferSummary&) noexcept;
+
+        //add one more expense
+        void addExpense(std::shared_ptr<Expense> expense);
+
+        //get the total_expense this person supposed to make
+        double& getTotalExpense();
+
+        const double& getTotalExpense() const;
+
+        //get how much this person paid at the first place
+        double& getPaymentMade();
+
+        const double& getPaymentMade() const;
+
+    };
+
+    struct Transfer {
+        std::string other;
+        //if the amount is positive, meaning "other" owe you
+        //otherwise, you owe "other"
+        double amount;
+        Transfer(const std::string _other, double _amount):
+            other(_other),
+            amount(_amount) {}
+
+    };
+
 } //AccountBalancer
+
+namespace std {
+    void swap(AccountBalancer::TransferSummary& left, 
+            AccountBalancer::TransferSummary& right) noexcept;
+} //std
 
 #endif
 
